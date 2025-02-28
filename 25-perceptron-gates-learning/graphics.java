ec541ac7bb173b9e29b9421d554e9514359cbd4f
Chloe Chen <26chloec@tas.edu.tw>
2025-02-28 21:21:00 +0800

    line prob

--- 25-perceptron-gates-learning/graphics.java
+++ 25-perceptron-gates-learning/graphics.java
@@ -10,6 +10,11 @@ public class graphics
 {
     // instance variables - replace the example below with your own
     private HashMap<String, Double> drawAns = new HashMap <String, Double>();
+    private double weight0;
+    private double weight1;
+    private double threshW;
+    private double output;
+    private double sample;
 
     /**
      * Constructor for objects of class graphics
@@ -20,8 +25,75 @@ public class graphics
     }
     
     public void drawLine(){
-        //0 = drawAns.get("Weight1")*drawAns.get("xInt1")+drawAns.get("Weight0")*drawAns.get("xInt0")+drawAns.get("threshW")
-        
+        //0 = drawAns.get("Weight0")*x+drawAns.get("Weight1")*y+drawAns.get("threshW")
+        //y = (-drawAns.get("threshW")-drawAns.get("Weight0")*x)/drawAns.get("Weight1")
+        if (output == 0){
+            StdDraw.setPenColor(StdDraw.RED);
+            if(sample == 0){
+                StdDraw.filledCircle(50, 50, 7); //0
+            }
+            else if (sample == 1){
+                StdDraw.filledCircle(500, 50, 7); //1
+            }
+            else if (sample == 2){
+                StdDraw.filledCircle(50, 500, 7); //2
+            }
+            else if (sample == 3){
+                StdDraw.filledCircle(500, 500, 7); //3
+            }
+        }
+        else if (output == 1){
+            StdDraw.setPenColor(StdDraw.GREEN);
+            if(sample == 0){
+                StdDraw.filledCircle(50, 50, 7); //0
+            }
+            else if (sample == 1){
+                StdDraw.filledCircle(500, 50, 7); //1
+            }
+            else if (sample == 2){
+                StdDraw.filledCircle(50, 500, 7); //2
+            }
+            else if (sample == 3){
+                StdDraw.filledCircle(500, 500, 7); //3
+            }
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        int x1 = -5;
+        int x2 = 5;
+        double y1 = (-threshW-weight0*x1)/weight1;
+        double y2 = (-threshW-weight0*x2)/weight1;
+        double scale = 1;
+        StdDraw.setPenRadius(0.002);
+        StdDraw.line(0, scale*y1, 600, scale*y2);
+        StdDraw.show();
+    }
+    
+    public void draw(){
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setCanvasSize(600,600);
+        StdDraw.setXscale(0,600);
+        StdDraw.setYscale(0,600);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        StdDraw.line(50, 50, 50, 550);
+        StdDraw.line(50, 50, 550, 50);
+        StdDraw.setPenRadius(0.005);
+        StdDraw.filledCircle(50, 50, 7); //0
+        StdDraw.filledCircle(500, 50, 7); //1
+        StdDraw.filledCircle(50, 500, 7); //2
+        StdDraw.filledCircle(500, 500, 7); //3
+    }
+    
+    public void drawFinal(){
+        StdDraw.setPenColor(StdDraw.BLUE);
+        int x1 = -5;
+        int x2 = 5;
+        double y1 = (-threshW-weight0*x1)/weight1;
+        double y2 = (-threshW-weight0*x2)/weight1;
+        double scale = 1;
+        StdDraw.setPenRadius(0.002);
+        StdDraw.line(0, scale*y1, 600, scale*y2);
+        StdDraw.show();
     }
 
     /**
@@ -30,10 +102,12 @@ public class graphics
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public void getHash(HashMap GA)
+    public void setInfo(double thW, double W0, double W1, double out, int j)
     {
-        // put your code here
-        drawAns = GA;
-        //System.out.println(drawAns);
+        threshW = thW;
+        weight0 = W0;
+        weight1 = W1;
+        output = out;
+        sample = j;
     }
 }
--- 25-perceptron-gates-learning/perceptron.java
+++ 25-perceptron-gates-learning/perceptron.java
@@ -24,10 +24,12 @@ public class perceptron
     //error = 0 false, error = 1 true
     //and
     private static int[] tar = {0, 0, 0, 1};
-    //or
-    //private static int[] tar = {0, 1, 1, 1};
     //nand
     //private static int[] tar = {1, 1, 1, 0};
+    //or
+    //private static int[] tar = {0, 1, 1, 1};
+    //nor
+    //private static int[] tar = {1, 0, 0, 0};
     private graphics gs = new graphics();
 
     /**
@@ -48,7 +50,9 @@ public class perceptron
 
     
     public void start(){
+        gs.draw();
         pertron();
+        gs.drawFinal();
     }
     
     public void deltaW(double W0, double W1, double tW, double output, double X0, double X1){
@@ -65,53 +69,38 @@ public class perceptron
         double val = 0;
         for(int i = 0; i<iteration; i++){
             for(int j = 0; j<4; j++){
-                if (!run[j]){
-                    target = tar[j];
-                    xInt = xSamples[j];
-                    val = weight1* xInt[1] + weight0*xInt[0] + threshW*threshX;
-                    System.out.println(j);
-                    if (val > 0){
-                        output = 1;
-                    }
-                    else{
-                        output = 0;
-                    }
-                    if (output != target){
-                        deltaW(weight0, weight1, threshW, output, xInt[0], xInt[1]);
-                        error = true;
-                        System.out.println("Output: " + output + " Target: " + target);
-                        System.out.println("Val: " + val);
-                        System.out.println("Error: " + error);
-                        System.out.println("Weight0: " + weight0 + " Weight1: " + weight1 + " ThreshW: " + threshW);
-                        System.out.println();
-                        drawAns.put("Output", output);
-                        drawAns.put("Weight0", weight0);
-                        drawAns.put("Weight1", weight1);
-                        drawAns.put("xInt0", (double)xInt[0]);
-                        drawAns.put("xInt1", (double)xInt[1]);
-                        drawAns.put("ThreshW", threshW);
-                        drawAns.put("ThreshX", threshX);
-                    }else{
-                        error = false;
-                        run[j] = true;
-                        System.out.println("Output: " + output + " Target: " + target);
-                        System.out.println("Val: " + val);
-                        System.out.println("Error: " + error);
-                        System.out.println("Weight0: " + weight0 + " Weight1: " + weight1 + " ThreshW: " + threshW);
-                        System.out.println();
-                        drawAns.put("Output", output);
-                        drawAns.put("Weight0", weight0);
-                        drawAns.put("Weight1", weight1);
-                        drawAns.put("xInt0", (double)xInt[0]);
-                        drawAns.put("xInt1", (double)xInt[1]);
-                        drawAns.put("ThreshW", threshW);
-                        drawAns.put("ThreshX", threshX);
-                        //drawAns.put("Error", 0.0);
-                    }
+                target = tar[j];
+                xInt = xSamples[j];
+                val = weight1* xInt[1] + weight0*xInt[0] + threshW*threshX;
+                System.out.println(j);
+                if (val > 0){
+                    output = 1;
                 }
-                gs.getHash(drawAns);
-                System.out.println(drawAns);
-                drawAns.clear();
+                else{
+                    output = 0;
+                }
+                if (output != target){
+                    deltaW(weight0, weight1, threshW, output, xInt[0], xInt[1]);
+                    error = true;
+                    System.out.println("Output: " + output + " Target: " + target);
+                    System.out.println("Val: " + val);
+                    System.out.println("Error: " + error);
+                    System.out.println("Weight0: " + weight0 + " Weight1: " + weight1 + " ThreshW: " + threshW);
+                    System.out.println();
+                }else{
+                    error = false;
+                    run[j] = true;
+                    System.out.println("Output: " + output + " Target: " + target);
+                    System.out.println("Val: " + val);
+                    System.out.println("Error: " + error);
+                    System.out.println("Weight0: " + weight0 + " Weight1: " + weight1 + " ThreshW: " + threshW);
+                    System.out.println();
+                    
+                }
+                gs.setInfo(threshW, weight0, weight1, output, j);
+                gs.drawLine();
+                //System.out.println(drawAns);
+                //drawAns.clear();
             }
         }
         error = true;
